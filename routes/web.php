fb43a835a1f493e420671b73d39d302a68be9df5
Benjamin Tumbleson <ben@83fa69d3-600e-432a-8a5f-35d0a7229f90.attlocal.net>
2025-03-21 22:17:10 -0700

    Public bucket testing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+
+
     return view('welcome');
+});
+
+
+Route::get('/url', function () {
+
+
+    $contents = Storage::disk('public2')->url('image.jpg');
+
+    dd($contents);
+});
+
+Route::get('/size', function () {
+
+
+    $contents = Storage::disk('public2')->size('image.jpg');
+
+    dd($contents);
 });
